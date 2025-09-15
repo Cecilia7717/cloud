1c12e1d17ede9368d8adaba6165c29f1a7860cc1
Cecilia <ceciliachen833@gmail.com>
2025-09-14 20:30:19 -0400

    ready to plot

--- test_multi.bash
+++ test_multi.bash
@@ -4,38 +4,17 @@
 export MASTER_ADDR=localhost
 export MASTER_PORT=29600
 export PYTHONPATH=/root:$PYTHONPATH
-python /SCRIPTS/test.py test-only \
-    --model ags_tiny_unet_50k \
-    --seed 23456 \
-    --quant_config 2 \
-    --data_path /pvc/ \
-    --csv_paths '{"train": "/pvc/2/train_noisy.csv", "test": "/pvc/2/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-2-23456/ags_tiny_unet_50k_backend-None-1_20250818-182321/best_model_75_test_F1=0.8269.pt" \
-    >> result_test_quan_2_noise_2.txt 2>&1
-
-cp result_test_quan_2_noise_2.txt /pvc/result_test_quan_2_noise_2.txt
-
-python /SCRIPTS/test.py test-only \
-    --model ags_tiny_unet_50k \
-    --seed 23456 \
-    --quant_config 2 \
-    --data_path /pvc/ \
-    --csv_paths '{"train": "/pvc/10/train_noisy.csv", "test": "/pvc/10/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-2-23456/ags_tiny_unet_50k_backend-None-1_20250818-182321/best_model_75_test_F1=0.8269.pt" \
-    >> result_test_quan_2_noise_10.txt 2>&1
-
-cp result_test_quan_2_noise_10.txt /pvc/result_test_quan_2_noise_10.txt
-
+# for model trained with quan 2 noise 2
 python /SCRIPTS/test.py test-only \
     --model ags_tiny_unet_50k \
     --seed 23456 \
     --quant_config 4 \
     --data_path /pvc/ \
-    --csv_paths '{"train": "/pvc/10/train_noisy.csv", "test": "/pvc/10/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-4-23456/ags_tiny_unet_50k_backend-None-1_20250818-182859/best_model_84_test_F1=0.8402.pt" \
-    >> result_test_quan_4_noise_10.txt 2>&1
+    --csv_paths '{"train": "/pvc/2/train_noisy.csv", "test": "/pvc/2/test_noisy.csv"}' \
+    --resume_from="/pvc/output-alcd-cloud-noisy-10-4/ags_tiny_unet_50k_backend-None-1_20250909-002426/best_model_93_test_F1=0.8390.pt" \
+    >> result_quan_4_gn_10_test_noise_2.txt 2>&1
 
-cp result_test_quan_4_noise_10.txt /pvc/result_test_quan_4_noise_10.txt
+cp result_quan_4_gn_10_test_noise_2.txt /pvc/result_quan_4_gn_10_test_noise_2.txt
 
 python /SCRIPTS/test.py test-only \
     --model ags_tiny_unet_50k \
@@ -43,32 +22,32 @@ python /SCRIPTS/test.py test-only \
     --quant_config 4 \
     --data_path /pvc/ \
     --csv_paths '{"train": "/pvc/5/train_noisy.csv", "test": "/pvc/5/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-4-23456/ags_tiny_unet_50k_backend-None-1_20250818-182859/best_model_84_test_F1=0.8402.pt" \
-    >> result_test_quan_4_noise_5.txt 2>&1
+    --resume_from="/pvc/output-alcd-cloud-noisy-10-4/ags_tiny_unet_50k_backend-None-1_20250909-002426/best_model_93_test_F1=0.8390.pt" \
+    >> result_quan_4_gn_10_test_noise_5.txt 2>&1
 
-cp result_test_quan_4_noise_5.txt /pvc/result_test_quan_4_noise_5.txt
+cp result_quan_4_gn_10_test_noise_5.txt /pvc/result_quan_4_gn_10_test_noise_5.txt
 
 python /SCRIPTS/test.py test-only \
     --model ags_tiny_unet_50k \
     --seed 23456 \
     --quant_config 4 \
     --data_path /pvc/ \
-    --csv_paths '{"train": "/pvc/2/train_noisy.csv", "test": "/pvc/2/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-4-23456/ags_tiny_unet_50k_backend-None-1_20250818-182859/best_model_84_test_F1=0.8402.pt" \
-    >> result_test_quan_4_noise_2.txt 2>&1
+    --csv_paths '{"train": "/pvc/10/train_noisy.csv", "test": "/pvc/10/test_noisy.csv"}' \
+    --resume_from="/pvc/output-alcd-cloud-noisy-10-4/ags_tiny_unet_50k_backend-None-1_20250909-002426/best_model_93_test_F1=0.8390.pt" \
+    >> result_quan_4_gn_10_test_noise_10.txt 2>&1
 
-cp result_test_quan_4_noise_2.txt /pvc/result_test_quan_4_noise_2.txt
+cp result_quan_4_gn_10_test_noise_10.txt /pvc/result_quan_4_gn_10_test_noise_10.txt
 
 python /SCRIPTS/test.py test-only \
     --model ags_tiny_unet_50k \
     --seed 23456 \
     --quant_config 6 \
     --data_path /pvc/ \
-    --csv_paths '{"train": "/pvc/10/train_noisy.csv", "test": "/pvc/10/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-6-23456/ags_tiny_unet_50k_backend-None-1_20250819-012631/best_model_81_test_F1=0.8416.pt" \
-    >> result_test_quan_6_noise_10.txt 2>&1
+    --csv_paths '{"train": "/pvc/2/train_noisy.csv", "test": "/pvc/2/test_noisy.csv"}' \
+    --resume_from="/pvc/output-alcd-cloud-noisy-10-quan-6/ags_tiny_unet_50k_backend-None-1_20250909-112342/best_model_93_test_F1=0.8392.pt" \
+    >> result_quan_6_gn_10_test_noise_2.txt 2>&1
 
-cp result_test_quan_6_noise_10.txt /pvc/result_test_quan_6_noise_10.txt
+cp result_quan_6_gn_10_test_noise_2.txt /pvc/result_quan_6_gn_10_test_noise_2.txt
 
 python /SCRIPTS/test.py test-only \
     --model ags_tiny_unet_50k \
@@ -76,32 +55,32 @@ python /SCRIPTS/test.py test-only \
     --quant_config 6 \
     --data_path /pvc/ \
     --csv_paths '{"train": "/pvc/5/train_noisy.csv", "test": "/pvc/5/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-6-23456/ags_tiny_unet_50k_backend-None-1_20250819-012631/best_model_81_test_F1=0.8416.pt" \
-    >> result_test_quan_6_noise_5.txt 2>&1
+    --resume_from="/pvc/output-alcd-cloud-noisy-10-quan-6/ags_tiny_unet_50k_backend-None-1_20250909-112342/best_model_93_test_F1=0.8392.pt" \
+    >> result_quan_6_gn_10_test_noise_5.txt 2>&1
 
-cp result_test_quan_6_noise_5.txt /pvc/result_test_quan_6_noise_5.txt
+cp result_quan_6_gn_10_test_noise_5.txt /pvc/result_quan_6_gn_10_test_noise_5.txt
 
 python /SCRIPTS/test.py test-only \
     --model ags_tiny_unet_50k \
     --seed 23456 \
     --quant_config 6 \
     --data_path /pvc/ \
-    --csv_paths '{"train": "/pvc/2/train_noisy.csv", "test": "/pvc/2/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-6-23456/ags_tiny_unet_50k_backend-None-1_20250819-012631/best_model_81_test_F1=0.8416.pt" \
-    >> result_test_quan_6_noise_2.txt 2>&1
+    --csv_paths '{"train": "/pvc/10/train_noisy.csv", "test": "/pvc/10/test_noisy.csv"}' \
+    --resume_from="/pvc/output-alcd-cloud-noisy-10-quan-6/ags_tiny_unet_50k_backend-None-1_20250909-112342/best_model_93_test_F1=0.8392.pt" \
+    >> result_quan_6_gn_10_test_noise_10.txt 2>&1
 
-cp result_test_quan_6_noise_2.txt /pvc/result_test_quan_6_noise_2.txt
+cp result_quan_6_gn_10_test_noise_10.txt /pvc/result_quan_6_gn_10_test_noise_10.txt
 
 python /SCRIPTS/test.py test-only \
     --model ags_tiny_unet_50k \
     --seed 23456 \
     --quant_config 8 \
     --data_path /pvc/ \
-    --csv_paths '{"train": "/pvc/10/train_noisy.csv", "test": "/pvc/10/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-8-23456/ags_tiny_unet_50k_backend-None-1_20250818-183924/best_model_114_test_F1=0.8516.pt" \
-    >> result_test_quan_8_noise_10.txt 2>&1
+    --csv_paths '{"train": "/pvc/2/train_noisy.csv", "test": "/pvc/2/test_noisy.csv"}' \
+    --resume_from="/pvc/output-alcd-cloud-noisy-10-quan-8/ags_tiny_unet_50k_backend-None-1_20250909-023033/best_model_90_test_F1=0.8408.pt" \
+    >> result_quan_8_gn_10_test_noise_2.txt 2>&1
 
-cp result_test_quan_8_noise_10.txt /pvc/result_test_quan_8_noise_10.txt
+cp result_quan_8_gn_10_test_noise_2.txt /pvc/result_quan_8_gn_10_test_noise_2.txt
 
 python /SCRIPTS/test.py test-only \
     --model ags_tiny_unet_50k \
@@ -109,18 +88,18 @@ python /SCRIPTS/test.py test-only \
     --quant_config 8 \
     --data_path /pvc/ \
     --csv_paths '{"train": "/pvc/5/train_noisy.csv", "test": "/pvc/5/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-8-23456/ags_tiny_unet_50k_backend-None-1_20250818-183924/best_model_114_test_F1=0.8516.pt" \
-    >> result_test_quan_8_noise_5.txt 2>&1
+    --resume_from="/pvc/output-alcd-cloud-noisy-10-quan-8/ags_tiny_unet_50k_backend-None-1_20250909-023033/best_model_90_test_F1=0.8408.pt" \
+    >> result_quan_8_gn_10_test_noise_5.txt 2>&1
 
-cp result_test_quan_8_noise_5.txt /pvc/result_test_quan_8_noise_5.txt
+cp result_quan_8_gn_10_test_noise_5.txt /pvc/result_quan_8_gn_10_test_noise_5.txt
 
 python /SCRIPTS/test.py test-only \
     --model ags_tiny_unet_50k \
     --seed 23456 \
     --quant_config 8 \
     --data_path /pvc/ \
-    --csv_paths '{"train": "/pvc/2/train_noisy.csv", "test": "/pvc/2/test_noisy.csv"}' \
-    --resume_from="/pvc/output-alcd-cloud-quan-8-23456/ags_tiny_unet_50k_backend-None-1_20250818-183924/best_model_114_test_F1=0.8516.pt" \
-    >> result_test_quan_8_noise_2.txt 2>&1
+    --csv_paths '{"train": "/pvc/10/train_noisy.csv", "test": "/pvc/10/test_noisy.csv"}' \
+    --resume_from="/pvc/output-alcd-cloud-noisy-10-quan-8/ags_tiny_unet_50k_backend-None-1_20250909-023033/best_model_90_test_F1=0.8408.pt" \
+    >> result_quan_8_gn_10_test_noise_10.txt 2>&1
 
-cp result_test_quan_8_noise_2.txt /pvc/result_test_quan_8_noise_2.txt
+cp result_quan_8_gn_10_test_noise_10.txt /pvc/result_quan_8_gn_10_test_noise_10.txt
