6b1a0e1955a1a0054f41961574c3ba1c2e596aae
Cecilia <ceciliachen833@gmail.com>
2025-11-03 09:24:15 -0500

    keep

--- test_noisefree.bash
+++ test_noisefree.bash
@@ -5,14 +5,25 @@ export MASTER_ADDR=localhost
 export MASTER_PORT=29600
 export PYTHONPATH=/root:$PYTHONPATH
 
-python /SCRIPTS/test_new.py run \
-     --model ags_tiny_unet_50k    \
+python SCRIPTS/test_normal.py eval \
+     --model ags_tiny_unet_50k \
      --seed 23456             \
      --quant_config 2             \
-     --data_path /pvc/            \
-     --csv_paths '{"train": "/pvc/train.csv", "test": "/pvc/valid.csv"}'  \
-     --resume_from="/pvc/output-alcd-cloud-2-new-saveall/best_checkpoint_93_test_F1=0.8279.pt"   \
+     --resume_from "/pvc/output-2/ags_tiny_unet_50k_backend-None-1_20251103-052223/best_checkpoint_72_test_F1=0.8324.pt" \
+     --data_path /pvc/ \
+     --csv_paths.train "/pvc/train.csv" \
+     --csv_paths.test "/pvc/valid.csv" \
      >> test_2_0.txt 2>&1
+
+python SCRIPTS/test_normal.py eval \
+     --model ags_tiny_unet_50k \
+     --seed 23456             \
+     --quant_config 2             \
+     --resume_from "/pvc/output-2/ags_tiny_unet_50k_backend-None-1_20251103-052223/best_checkpoint_72_test_F1=0.8324.pt" \
+     --data_path /pvc/ \
+     --csv_paths.train "/pvc/2_new_sap/train_noisy.csv" \
+     --csv_paths.test "/pvc/2_new_sap/test_noisy.csv" \
+     >> test_2_2_sap.txt 2>&1
 
 python /SCRIPTS/test_new.py run \
      --model ags_tiny_unet_50k    \
